0ed65db5094253c66ff12d0122de17d5f218772d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:35:35 +0200

    Serra

--- Serra/Program.cs
+++ Serra/Program.cs
@@ -10,15 +10,15 @@ namespace Serra
             double grado = double.Parse(Console.ReadLine());
             if (grado >= 18)
             {
-                Console.WriteLine("La temperatura all'interno della serra è normale ");
+                Console.WriteLine("La temperatura all'interno della serra è normale");
 
             }else if (grado>5 && grado < 18)
             {
-                Console.WriteLine("La temperatura all'interno della serra non è normale e vi è una situazione di pericolo ");
+                Console.WriteLine("La temperatura all'interno della serra non è normale e vi è una situazione di pericolo");
 
             }else if (grado < 5)
             {
-                Console.WriteLine("La temperatura all'interno della serra non è normale e si potrebbero avere danni irreparabili ");
+                Console.WriteLine("La temperatura all'interno della serra non è normale e si potrebbero avere danni irreparabili");
 
             }
             Console.ReadLine();
