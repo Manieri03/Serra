84b1f07d60edf1a278ab19badcd0b9a4b9f86dd0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:35:20 +0200

    Add project files.

---
+++ Serra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Serra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanti gradi ci sono all'interno della serra?");
+            double grado = double.Parse(Console.ReadLine());
+            if (grado >= 18)
+            {
+                Console.WriteLine("La temperatura all'interno della serra è normale ");
+
+            }else if (grado>5 && grado < 18)
+            {
+                Console.WriteLine("La temperatura all'interno della serra non è normale e vi è una situazione di pericolo ");
+
+            }else if (grado < 5)
+            {
+                Console.WriteLine("La temperatura all'interno della serra non è normale e si potrebbero avere danni irreparabili ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
